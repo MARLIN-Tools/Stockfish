638f4e9c64982298a3ce1557a47dfe6b011473f1
SpaceFaceGG <jubilantjanitorthrowaway@gmail.com>
2026-03-24 23:40:07 -0500

    eval: remove nnue complexity adjustment

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -72,11 +72,6 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
         smallNet                   = false;
     }
 
-    // Blend optimism and eval with nnue complexity
-    int nnueComplexity = std::abs(psqt - positional);
-    optimism += optimism * nnueComplexity / 476;
-    nnue -= nnue * nnueComplexity / 18236;
-
     int material = 534 * pos.count<PAWN>() + pos.non_pawn_material();
     int v        = (nnue * (77871 + material) + optimism * (7191 + material)) / 77871;
 
